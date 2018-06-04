424b32f496a7976becfdf0c4f35f1a364d1336c7
1712244 <lucasalvesoliva@gmail.com>
2018-06-04 20:08:41 -0300

    Criando Contexto e alterando algumas coisas pra public

--- ControleAtividades/Controllers/DAL/Contexto.cs
+++ ControleAtividades/Controllers/DAL/Contexto.cs
@@ -1,13 +1,23 @@
-﻿using System;
+﻿using Modelos;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Controllers.DAL
 {
-    class Contexto
+    class Contexto : DbContext
     {
+        public Contexto() : base("strConn")
+        {
 
+        }
+        public DbSet<Usuario> Usuarios { get; set; }
+
+        public DbSet<Categoria> Categoria { get; set; }
+
+        public DbSet<Atividade> Atividade { get; set; }
     }
 }
